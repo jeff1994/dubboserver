a31eef6c62691c3646a444025de339225e35604a
jeff_zyc <1011407162@qq.com>
2017-08-09 23:27:31 +0800

    初始化

---
+++ src/main/java/com/jeff/Service/PersonService.java
@@ -0,0 +1,8 @@
+package com.jeff.Service;
+
+/**
+ * Created by zyc on 2017/8/9 0009.
+ */
+public interface PersonService {
+     String  returnPersonName();
+}
---
+++ src/main/java/com/jeff/ServiceImp/PersonServiceImp.java
@@ -0,0 +1,14 @@
+package com.jeff.ServiceImp;
+
+import com.jeff.Service.PersonService;
+
+/**
+ * Created by zyc on 2017/8/9 0009.
+ */
+public class PersonServiceImp implements PersonService {
+
+    public  String returnPersonName(){
+        return "张三";
+    }
+
+}
